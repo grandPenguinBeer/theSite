748be89c24df51311f1674077f83285462192288
Cyrus Shahrivar <cyrus.shahrivar@gmail.com>
2017-11-30 09:15:02 -0500

    add mobile intro styling; update header styling and smooth scroll

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,25 @@
-$(document).ready(function () {
-   $(window).on('scroll', function () {
-        if ($(this).scrollTop() > 50) {
-            $('header').css('background-color', 'black')
-        } else {
-            $('header').css('background-color', 'transparent')
-        }
-   });
+$(document).ready(function() {
+    var scrollOptions;
 
-   var scroll = new SmoothScroll('a[href*="#"]', {
-        speed: 1000,
-        offset: 83
-    });
+    if (window.innerWidth > 1000) {
+        $(window).on('scroll', function() {
+            if ($(this).scrollTop() > 50) {
+                $('header').css('background-color', 'black')
+            } else {
+                $('header').css('background-color', 'transparent')
+            }
+        });
+
+        scrollOptions = {
+            speed: 1000,
+            offset: 83
+        };
+    } else {
+        scrollOptions = {
+            speed: 1000,
+            offset: 60
+        };
+    }
+
+    var scroll = new SmoothScroll('a[href*="#"]', scrollOptions);
 });
